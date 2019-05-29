6e23b2a040d54b9be484ca064a3c0794bc512e0d
Sebastian Alejandro <sebastian1231alejandro@gmail.com>
2019-05-29 10:02:11 -0400

    changes

---
+++ Password.java
@@ -0,0 +1,30 @@
+
+/**
+ * @author Sebastian Alejandro, Borjan Palankov
+ * @version 2019-05-29
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Password {
+    public static void main(String[] args) {
+
+        public class genComplex {
+            /* 
+             * This will generate a complex password, such as "S3AZ$C@Y7z$&!x" 
+             */
+            final String CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            final String SPECIAL_CHARS = "!@#$%^&*()_-.";
+            final String NUMBERS = "1234567890";
+        }
+
+        public class genSimple {
+            /*
+             * This will generate a simple pasword, such as "emperor blaming broadband" or
+             * "reusable-hurled-eggbeater"
+             */
+            char separator = "";
+        }
+    }
+}
--- PasswordGen.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Sebastian Alejandro, Borjan Palankov
- * @version 2019-05-29
- */
-public class PasswordGen {
-    public static void main (String[] args) {
-        
-    }
-}
