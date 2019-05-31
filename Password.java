fe79d929da4dd52ddc19249181feb510822aad64
Sebastian Alejandro <sebastian1231alejandro@gmail.com>
2019-05-30 22:13:05 -0400

    progess on YAPG

--- Password.java
+++ Password.java
@@ -1,23 +1,29 @@
 
 /**
+ * YAPG - Yet Another Password Generator
+ * A password generator that barely anyone (no one) will use
+ * 
  * @author Sebastian Alejandro, Borjan Palankov
- * @version 2019-05-29
+ * @version 0.0.1
  */
 
-import java.util.Scanner;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Password {
     public static void main(String[] args) {
 
-        int selection = 0;
+        String selection = "";
+        boolean running = true;
 
         Scanner in = new Scanner(System.in);
-        
+
         System.out.printf("Hello, and Welcome to: \n\n");
-        
-        System.out.println(" #     #    #    ######   ##### ");
-        System.out.println("  #   #    # #   #     # #     #");
+
+        System.out.println(" #     #    #    ######   ##### "); /* Please appreciate this, it took me like 30 mins */
+        System.out.println("  #   #    # #   #     # #     #"); 
         System.out.println("   # #    #   #  #     # #      ");
         System.out.println("    #    #     # ######  #  ####");
         System.out.println("    #    ####### #       #     #");
@@ -25,50 +31,79 @@ public class Password {
         System.out.println("    #    #     # #        ##### ");
 
         System.out.println("\nWould you like a complex or simple  password?");
-        System.out.println("Please press \"1\" for  a complex, or \"2\" for a simple password");
+        System.out.println("Please type \"complex\" for a complex password, or \"simple\" for a simple password, or press \"Q\" to quit.");
 
-
-
-        if (in.hasNextInt()){
-            selection = in.nextInt();
-            
-            if (selection == 1) {
-                System.out.println("Complex password selected.");
-                in.close();
-            }
-
-            if (selection == 2) {
-                System.out.println("Simple password selected");
-                in.close();
-            }
-
-            if (selection != 1 || selection != 2) {
-                System.out.println("Unknown");
-            }
-        }  
+        while (running) { /* will constantly run until the user exits */
+            if (in.hasNext()) {
+                selection = in.next();
+                if (selection.equalsIgnoreCase("complex")) {
+                    System.out.println("Complex password selected.");
+                    System.out.println("Your password is ");
+                    genComplex();
+                } else if (selection.equalsIgnoreCase("simple")) {
+                    System.out.println("Simple password selected");
+                    System.out.print("Your password is ");
+                    genSimple();
+                } else if (selection.equalsIgnoreCase("q")) {
+                    System.out.println("Exiting YAPG.");
+                    running = false;
+                } else {
+                    System.out.println("Unknown Input. Please put in a correct password type, either \"complex\" or \"simple\". ");
+                }
+            } 
+        }
     }
 
-    public String genComplex ( ) {
-        /* 
-         * This will generate a complex password, such as "S3AZ$C@Y7z$&!x" 
+    private static String genComplex() {
+        /*
+         * This will generate a complex password, such as "S3AZ$C@Y7z$&!x"
          */
         String finalpasswd = "";
-        final String CHARS_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        final String CHARS_LOWER = "abcdefghijklmnopqrstuvwxyz";
-        final String SPECIAL_CHARS = "!@#$%^&*()_-.";
-        final String NUMBERS = "1234567890";
+        // using CHAR_LOWER, gen a random num (0 or 1), and either keep it as it is, or use .toUpperCase()
+        final String[] CHARS = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q","r", "s", "t", "u", "v", "w", "x", "y" "z" };
+        final String[] SPECIAL_CHARS = { "!", "@", "#", "$", "%", "^", "&", ",", "(", ")", "_", "-", ",", "." };
+        final String[] NUMBERS = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+
+        Random rand = new Random();
 
         return finalpasswd;
     }
 
-    public String genSimple() {
+    private static String genSimple() {
         /*
          * This will generate a simple pasword, such as "emperor blaming broadband" or
          * "reusable-hurled-eggbeater"
          */
-        String finalpasswd = "";
+
+        String finalpasswd = " ";
         String separator;
 
+        ArrayList<String> dict = readDict();
+
+        if (dict == null) { /* when dict is null, program exits */
+            System.err.print("Could not find wordlist, exiting.");
+            System.exit(0);
+        }
+
         return finalpasswd;
+    }
+
+    private static ArrayList<String> readDict () {
+
+        ArrayList<String> dict = new ArrayList();
+        /* If the file "wordlist.txt" could not be found, it will return null, or the actual array if the file can be found */
+        /* Should it fail, it will let the user know, and fail. */
+        try {
+            Scanner reader = new Scanner(new File("wordlist.txt"));  
+
+            while (reader.hasNext()) {
+                dict.add(reader.nextLine());
+            }
+
+            return dict;
+        } catch (Exception e) {
+            System.out.printf("Could not find file\n");
+        }
+        return null;
     }
 }
--- README.md
+++ README.md
@@ -1,23 +1,28 @@
 <div align="center">
-    <h1> YAPG </h1>
-    <h5>Yet Another Password Generator</h5>
+    <h1>YAPG</h1>
+    <h5>Yet <i>Another</i> Password Generator</h5>
     <h6><i>A password generator that barely anyone (no one) will use</i></h6>
 </div>
 
 ## Made by
+
 * Borjan Palankov
 * Sebastian Alejandro
 
-## Our Idea:
+## Our Idea
+
 Create a password generator that can make simple (`powdery-exponent-underhand`) and complex (`@nD6V&c#*@JJ7%`) passwords
 
-## Goals:
+## Goals
+
 - [ ] Get user input to ask how many characters in their password, what type of password, and the number of passwords.
-    - [ ] How many characters? (default 12 if complex, 4 words if simple)
-    - [ ] Type of password? 
-    - [ ] Number of passwords? (default 1)
-- [ ] Generate Simple passwords 
-    - [ ] Use an array with random words, and select them using a random number
+  - [ ] How many characters? (default 12 if complex, 4 words if simple)
+  - [x] Type of password?
+  - [ ] Number of passwords? (default 1)
+- [ ] Generate Simple passwords
+  - [ ] Use an array with random words, and select them using a random number
+  - [x] Create a word list somehow (done with Scanner)
+  - [ ] Ask if the user wants spaces or dashes as a separator (default spaces)
 - [ ] Generate complex passwords
-    - [ ] Use substrings to choose randomly from `CHARS`,`SPECIAL_CHARS` and `NUMBERS`.
-         - [ ] Concatonate these random characters together to make a password.
+  - [ ] Use arrays to choose randomly from `CHARS`,`SPECIAL_CHARS` and `NUMBERS`.
+    - [ ] Concatonate these random characters together to make a password.
